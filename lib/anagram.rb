85c9f5436f3f4a7cfd23b780da9c9a8657bc0620
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-28 18:40:53 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,7 +13,7 @@ class Anagram
   def match(word_list)
     anagram = []
     word_list.each do |word_to_check|
-       if @word.chars.all? {|char| word_to_check.include?(char)}
+       if @word.chars.all? {|char| word_to_check.include?(char) if @word.size == word_to_check.size}
          anagram << word_to_check
        end
     end
