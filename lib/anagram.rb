def83fbafa6bae34e99866aff48a18136c1eb2da
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-28 18:41:17 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+require 'pry'
+
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_list)
+    anagram = []
+    word_list.each do |word_to_check|
+       if @word.chars.all? {|char| word_to_check.include?(char) if @word.size == word_to_check.size}
+         anagram << word_to_check
+       end
+    end
+    anagram
+  end
+
+end
